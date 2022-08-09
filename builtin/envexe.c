fd72af1c0ba5e5598d28edb58803a683e60c9a4e
Ahmet Hakan Güneş <ahmethakangunes24@gmail.com>
2022-08-09 12:18:34 +0300

    envexe

--- builtin/envexe.c
+++ builtin/envexe.c
@@ -6,7 +6,7 @@
 /*   By: agunes <agunes@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/08 14:00:42 by agunes            #+#    #+#             */
-/*   Updated: 2022/08/08 17:09:37 by agunes           ###   ########.fr       */
+/*   Updated: 2022/08/09 12:14:01 by agunes           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,13 +23,19 @@ int	ft_envexe(void)
 	{
 		if (!ft_strncmp(g_shell->commandlist[0] + 1, g_shell->env[i], len))
 		{
-			g_shell->envexe = ft_strdup(g_shell->env[i] + (len + 1));
-			ft_builtfree();
-			runcommand(g_shell->envexe);
-			free(g_shell->envexe);
-			break ;
+			if (g_shell->env[i][len] == '=')
+			{
+				g_shell->free_flag = 1;
+				ft_builtfree();
+				g_shell->envexe = ft_strdup(g_shell->env[i] + (len + 1));
+				runcommand(g_shell->envexe);
+				free(g_shell->envexe);
+				break ;
+			}
 		}
 		i++;
 	}
+	ft_builtfree();
+	g_shell->free_flag = 1;
 	return (1);
 }
