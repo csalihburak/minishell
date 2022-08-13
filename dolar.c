26c489a0a7fd22f4e3d4c759fa7b31aa48bf77f7
Ahmet Hakan Güneş <ahmethakangunes24@gmail.com>
2022-08-14 00:08:32 +0300

    $ update

--- dolar.c
+++ dolar.c
@@ -6,7 +6,7 @@
 /*   By: agunes <agunes@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/13 16:04:49 by agunes            #+#    #+#             */
-/*   Updated: 2022/08/13 23:51:39 by agunes           ###   ########.fr       */
+/*   Updated: 2022/08/14 00:07:48 by agunes           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,7 +45,7 @@ char	*cmdlistup(char *command)
 		buff = ft_strjoin(buff, (g_shell->env[g_shell->envflag] + \
 		findindex(g_shell->env[g_shell->envflag], '=')));
 	else
-		buff = ft_strjoin(buff, command + dolarfind(command));
+		buff = ft_strjoin(buff, command + dolarfind(command) + 1);
 	free(command);
 	return (buff);
 }
