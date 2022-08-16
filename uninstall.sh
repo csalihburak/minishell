4e5a1759168dc25dd5b96b885ab379323b0401c3
Ahmet hakan Gunes <agunes@k1m34s02.42istanbul.com.tr>
2022-08-16 05:12:33 +0300

    uninstall.sh update

--- uninstall.sh
+++ uninstall.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
 #agunes
-shell_f=$(echo -n "$SHELL" | awk -F / '{print $3}')
-shell_f="${HOME}/.${shell_f}rc"
-
-if grep "alias minishell='~/./minishell'" <"$shell_f" &>/dev/null && ls "$HOME"/./minishell &>/dev/null; then
-    sed -i '' -e '$ d' ~/.zshrc
-    exit 0
-
-fi
-
-if ! grep "alias minishell='~/./minishell'" <"$shell_f" &>/dev/null && ls "$HOME"/./minishell &>/dev/null; then
-    exit 0
-
-fi
+sed -i '' '/alias minishell=/d' ~/.zshrc
