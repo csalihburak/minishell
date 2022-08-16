13a4603572c5eea054866a53ff6e897094407403
Ahmet hakan Gunes <agunes@k1m34s02.42istanbul.com.tr>
2022-08-17 02:04:18 +0300

    save files

---
+++ main.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: scoskun <scoskun@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/17 02:00:53 by scoskun           #+#    #+#             */
+/*   Updated: 2022/08/17 02:01:39 by scoskun          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+void	prompt(void)
+{
+	g_shell->prompt = ft_prompt();
+	g_shell->command = readline(g_shell->prompt);
+	free(g_shell->prompt);
+}
+
+void	start(void)
+{
+	ft_signal();
+	prompt();
+	if (g_shell->command != NULL)
+	{
+		if (g_shell->command[0] > 0)
+		{
+			ft_free();
+			ft_parser();
+			dolar();
+			if (g_shell->commandlist[0])
+				runcommand(g_shell->commandlist[0]);
+			add_history(g_shell->command);
+			free(g_shell->command);
+		}
+		else
+			free(g_shell->command);
+	}
+	if (g_shell->command == NULL)
+		exit(0);
+}
+
+int	main(int argc, char **argv, char **env)
+{
+	(void)argc;
+	(void)argv;
+	g_shell = malloc(sizeof(t_shell));
+	exportenvcpy(env);
+	while (1)
+		start();
+	return (0);
+}
