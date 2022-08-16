d6429c0870b81259e4404e7f3e99dd422204225d
Ahmet hakan Gunes <agunes@k1m34s02.42istanbul.com.tr>
2022-08-16 05:43:54 +0300

    install.sh update

--- install.sh
+++ install.sh
@@ -4,11 +4,6 @@ shell_f=$(echo -n "$SHELL" | awk -F / '{print $3}')
 shell_f="${HOME}/.${shell_f}rc"
 cp -f ./minishell "$HOME"
 
-
-if grep "alias minishell='~/./minishell'" <"$shell_f" &>/dev/null && ls "$HOME"/./minishell &>/dev/null; then
-    exit 0
-fi
-
 if ! grep "alias minishell='~/./minishell'" <"$shell_f" &>/dev/null; then
 	echo "alias minishell='~/./minishell'" >>"$shell_f"
 	exit 0
