22a24e7a0432786e1a94e6171df272bd755534b5
Salih burak Coskun <scoskun@k1m35s01.42istanbul.com.tr>
2022-08-24 11:03:47 +0300

    finished < operator

--- file_operators/pipe_handling.c
+++ file_operators/pipe_handling.c
@@ -6,7 +6,7 @@
 /*   By: scoskun <scoskun@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/17 14:49:03 by scoskun           #+#    #+#             */
-/*   Updated: 2022/08/23 17:48:54 by scoskun          ###   ########.fr       */
+/*   Updated: 2022/08/24 11:02:20 by scoskun          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,22 +32,22 @@ void	less_op_handling(t_op *file)
 {
 	int		i;
 	int		pid;
-	int		len;
 	char	**temp;
 
 	i = -1;
-	len = dblen2(file->cmd_list);
 	while (file->ops[++i])
 	{
 		if (!ft_strcmp(file->ops[i], "<"))
 		{
-			temp = ft_split_quote(file->cmd_list[len - 1], ' ');
+			temp = ft_split_quote(file->command, ' ');
 			pid = fork();
 			if (pid == 0)
 			{
-				file->fd = open(temp[dblen2(temp) - 1], O_WRONLY);
+				file->fd = open(temp[dblen2(temp) - 1], O_RDONLY);
 				dup2(file->fd, 0);
+				close(file->fd);
 				create_ops(file, file->cmd_list[0]);
+				kill(getpid(), SIGTERM);
 			}
 			else
 				wait(NULL);
